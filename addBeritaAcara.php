d0104b576d341a63d688515c45b90c44ee0831ec
spaceJump13 <sadinokevin9@gmail.com>
2023-11-11 03:09:04 +0700

    ADD BERITA ACARA YANG DIPAKE

--- addBeritaAcara.php
+++ addBeritaAcara.php
@@ -15,13 +15,10 @@ $nama_dosen = $_SESSION['username'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-    <!-- Bootstrap CSS and JavaScript -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-
+    <script src="jquery-3.7.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <title>Add Berita Acara</title>
     <style>
         .checkbox-lg .form-check-input{
@@ -143,7 +140,7 @@ $nama_dosen = $_SESSION['username'];
         <div class="container"> <!--<div class="container my-5">-->
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 style="text-align: center; color: #0B6977" class="text-uppercase">Tambah Berita</h1>
+                    <h1 style="text-align: center; color: #0B6977" class="text-uppercase">Tambah Berita Acara</h1>
                 </div>
             </div>
     
@@ -159,12 +156,18 @@ $nama_dosen = $_SESSION['username'];
                                         <?php
                                             $sql_dosen = "SELECT * FROM data_dosen WHERE nama LIKE '%$nama_dosen%'";
                                             $result_dosen = mysqli_query($conn, $sql_dosen);
-                                            $row = mysqli_fetch_assoc($result_dosen);
+
+                                            if(mysqli_num_rows($result_dosen) > 0){
+                                                $row = mysqli_fetch_assoc($result_dosen);
+                                                $ketua_penguji = $row['nama'];
+                                            }
+                                            else{
+                                                $ketua_penguji = "?";
+                                            }
                                         ?>
-                                    <input type="text" name="ketuaPenguji" id="ketuaPenguji" value="<?php echo $row['nama']; ?>" style="background-color: white;" class="form-control" placeholder="Dosen Penilai" readonly>
+                                    <input type="text" name="ketuaPenguji" id="ketuaPenguji" value="<?php echo $ketua_penguji; ?>" style="background-color: white;" class="form-control" placeholder="Dosen Penilai" readonly>
                                 </div>
                 
-    
                                 <div class="col-lg-2">
                                     <div class="form-check checkbox-lg" style="margin-top: 40px;">
                                         <input class="form-check-input" type="checkbox" value="" id="kehadiranKetuaPenguji" name="kehadiranKetuaPenguji">
@@ -431,6 +434,24 @@ $nama_dosen = $_SESSION['username'];
                     </div>
                 </div>
             </form>
+
+            <div class="modal fade" id="notKetuaPengujiModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalCenterTitle">Anda bukan Ketua Penguji!</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="" method="POST" enctype="multipart/form-data">
+                        <div class="modal-body ">
+                            <h5>Tidak ada mahasiswa yang mempunyai anda sebagai ketua penguji!</h5>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-outline-red" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
             
             <div class="modal fade" id="emptyField" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div class="modal-dialog">
@@ -476,20 +497,26 @@ $nama_dosen = $_SESSION['username'];
             // ketua penguji
             var ketuaPenguji = $('#ketuaPenguji').val();
             console.log(ketuaPenguji);
-            $.ajax({
-                url: "ajax_berita_acara.php",
-                type: "POST",
-                data:{
-                    tanda: "Mahasiswa",
-                    id:ketuaPenguji
-                },
-                success:function(respond){
-                    $("#nama_mhs").html(respond);
-                },
-                error:function(){
-                    alert("gagal");
-                }
-            })
+
+            if (ketuaPenguji == "?"){
+                $('#notKetuaPengujiModal').modal('show');
+            }
+            else{
+                $.ajax({
+                    url: "ajax_berita_acara.php",
+                    type: "POST",
+                    data:{
+                        tanda: "Mahasiswa",
+                        id:ketuaPenguji
+                    },
+                    success:function(respond){
+                        $("#nama_mhs").html(respond);
+                    },
+                    error:function(){
+                        alert("gagal");
+                    }
+                })
+            }
             
             // dosen penguji
             $('#nama_mhs').on('change', function(){
